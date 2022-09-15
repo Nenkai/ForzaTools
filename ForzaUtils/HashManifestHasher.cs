e2ef6060c78b19c4d207cce0b8a40ab8b93ce20f
Nenkai <Nenkai@users.noreply.github.com>
2022-09-15 22:02:18 +0200

    Add some hash utils

---
+++ ForzaUtils/HashManifestHasher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForzaUtils
+{
+    internal class HashManifestHasher
+    {
+        static uint StringHashIter(string name, bool caseInsensitive, uint hash = 5381)
+        {
+            if (name is not null)
+            {
+                foreach (char c in name)
+                {
+                    char use_c = c;
+                    if (caseInsensitive)
+                        use_c = c.ToString().ToUpper()[0];
+                    uint int_c = Convert.ToUInt32(c);
+                    hash ^= int_c + (hash >> 2) + 32 * hash;
+                }
+            }
+            return hash;
+        }
+
+        // Intended for HashManifest.xml
+        // Input would be path to file, i.e media\Audio\ModularCars\VW_GolfGTI_83-Engine.xml
+        public static ulong StringHash64(string name, bool caseInsensitive)
+        {
+            var hash = StringHashIter(name, caseInsensitive);
+            return hash | Convert.ToUInt64(StringHashIter(name, caseInsensitive, hash)) << 32;
+        }
+    }
+}
---
+++ ForzaUtils/LocaleStringTableStringHasher.cs
@@ -0,0 +1,34 @@
+﻿namespace ForzaUtils
+{
+    /// <summary>
+    /// From Reverse engineered code, used in .str files
+    /// Also used in unstripped databases for string columns
+    /// Normally two hashes appended, example:
+    /// - Data_Car = 0x434455F2 (Table)
+    /// - IDS_DisplayName_247 = 0x71A34BB8 (Localize Name)
+    /// - 0x434455F271A34BB8 -> _&4847093598734470072 (converted decimal into string)
+    /// 
+    /// Usage example is: HashString("IDS_Mode1String_2990", 32, true);
+    /// Seems to be always 32
+    /// </summary>
+    public class LocaleStringTableStringHasher
+    {
+        static long HashString(string str, int bitSize, bool caseInsensitive)
+        {
+            long v = (((long)1) << bitSize) - 1;
+            long res = v;
+            byte v7 = (byte)(bitSize - 7);
+
+            for (var i = 0; i < str.Length; i++)
+            {
+                char c = str[i];
+                if (!caseInsensitive && char.IsLetter(c) && char.IsUpper(c))
+                    c = char.ToLower(c);
+
+                res = v & (((c ^ res) << 7) | ((c ^ res) >> v7));
+            }
+
+            return res;
+        }
+    }
+}
