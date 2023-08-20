0a6f5ffe55465386f7dd6dc180a0a6f424be4f81
Nenkai <Nenkai@users.noreply.github.com>
2023-08-20 03:40:02 +0200

    XML <-> BXML

--- BXML/BXML.cs
+++ BXML/BXML.cs
@@ -7,98 +7,235 @@ using System.IO;
 
 using System.Xml;
 using Syroot.BinaryData;
+using System.Xml.Linq;
 
 namespace BXML
 {
     public class BXML
     {
-        public List<string> Strings { get; set; }
+        public List<string> Strings { get; set; } = new List<string>();
         public CXMLNode RootNode { get; set; }
-        
-        public static BXML ReadFile(string file)
+
+        public const uint MAGIC = 0x4C4D5842; // BXML
+        public const byte VERSION_CURRENT = 2;
+
+        public void LoadFromBinaryFile(string file)
         {
             using var fs = new FileStream(file, FileMode.Open);
             using var bs = new BinaryStream(fs);
 
-            var bxml = new BXML();
+            LoadFromStream(fs);
+        }
+
+        public void LoadFromStream(Stream stream)
+        {
+            using var bs = new BinaryStream(stream);
 
             uint magic = bs.ReadUInt32();
-            if (magic != 0x4C4D5842)
+            if (magic != MAGIC)
                 throw new InvalidDataException("Not a BXML file. (Magic did not match)");
-            
+
             byte version = bs.Read1Byte();
-            if (version > 2)
+            if (version > VERSION_CURRENT)
                 throw new InvalidDataException($"BXML Version is above maximum supported ({version} > 2).");
 
             int stringCount = bs.ReadInt32();
             int stringTableSize = bs.ReadInt32();
 
-            bxml.Strings = new List<string>(stringCount);
+            Strings = new List<string>(stringCount);
             for (var i = 0; i < stringCount; i++)
-                bxml.Strings.Add(bs.ReadString(StringCoding.Int16CharCount));
+                Strings.Add(bs.ReadString(StringCoding.Int16CharCount));
 
             byte rootFlag = bs.Read1Byte();
-            bxml.RootNode = bxml.ReadNode(bs);
+            RootNode = ReadBinaryXmlNode(bs);
+        }
 
-            return bxml;
+        public static void SerializeXMLToBXML(string xmlFile, string outputPath)
+        {
+            var doc = new XmlDocument();
+            doc.Load(xmlFile);
+
+            var bxml = new BXML();
+            bxml.CreateFromXmlNode(doc);
+
+            using var fs = new FileStream(outputPath, FileMode.Create);
+            bxml.Serialize(fs);
+        }
+
+        private CXMLNode RegisterXmlNode(XmlNode node)
+        {
+            var bxmlNode = new CXMLNode();
+            bxmlNode.Name = node.Name;
+            bxmlNode.Flags |= CXmlNodeFlags.IsNode;
+
+            foreach (XmlAttribute attr in node.Attributes)
+            {
+                bxmlNode.Flags |= CXmlNodeFlags.HasAttributes;
+                bxmlNode.Attributes.Add(new CXMLAttribute(attr.Name, attr.Value));
+            }
+
+            foreach (XmlNode childNode in node.ChildNodes)
+            {
+                bxmlNode.Flags |= CXmlNodeFlags.HasChildNodes;
+                var childBxmlNode = RegisterXmlNode(childNode);
+                bxmlNode.ChildNodes.Add(childBxmlNode);
+            }
+
+            return bxmlNode;
+        }
+
+        public void CreateFromXmlNode(XmlDocument doc)
+        {
+            foreach (XmlNode n in doc.ChildNodes)
+            {
+                if (n is XmlDeclaration)
+                    continue;
+
+                RootNode = RegisterXmlNode(n);
+            }
+
+            BuildStringList(RootNode);
+            Strings.Sort(AlphaNumStringComparer.Default);
+        }
+
+        private void BuildStringList(CXMLNode node)
+        {
+            AddString(node.Name);
+            foreach (CXMLAttribute attr in node.Attributes)
+            {
+                AddString(attr.Name);
+                AddString(attr.Value);
+            }
+
+            foreach (CXMLNode childNode in node.ChildNodes)
+            {
+                BuildStringList(childNode);
+            }
+        }
+
+
+        /// <summary>
+        /// Serializes to binary to the provided stream.
+        /// </summary>
+        /// <param name="stream"></param>
+        public void Serialize(Stream stream)
+        {
+            using var bs = new BinaryStream(stream, ByteConverter.Little);
+            bs.WriteUInt32(MAGIC);
+            bs.WriteByte(VERSION_CURRENT);
+            bs.WriteInt32(Strings.Count);
+            bs.Position += 4;
+
+            long baseTableOffset = bs.Position;
+            for (int i = 0; i < Strings.Count; i++)
+                bs.WriteString(Strings[i], StringCoding.Int16CharCount);
+            long endStringTableOffset = bs.Position;
+
+            bs.Position = baseTableOffset - 4;
+            bs.WriteUInt32((uint)(endStringTableOffset - baseTableOffset));
+            bs.Position = endStringTableOffset;
+
+            bs.WriteByte(1);
+            SerializeNode(bs, RootNode);
+        }
+
+        private void SerializeNode(BinaryStream bs, CXMLNode node)
+        {
+            bs.WriteByte((byte)node.Flags);
+            SerializeStringIndex(bs, node.Name);
+
+            if (node.Flags.HasFlag(CXmlNodeFlags.HasAttributes))
+            {
+                bs.WriteByte((byte)node.Attributes.Count);
+
+                foreach (CXMLAttribute attr in node.Attributes)
+                {
+                    SerializeStringIndex(bs, attr.Name);
+                    SerializeStringIndex(bs, attr.Value);
+                }
+            }
+
+            if (node.Flags.HasFlag(CXmlNodeFlags.HasChildNodes))
+            {
+                bs.WriteUInt16((ushort)node.ChildNodes.Count);
+
+                foreach (CXMLNode childNode in node.ChildNodes)
+                    SerializeNode(bs, childNode);
+            }
+        }
+
+        private void SerializeStringIndex(BinaryStream bs, string str)
+        {
+            int index = Strings.IndexOf(str);
+            if (Strings.Count > ushort.MaxValue)
+            {
+                bs.WriteInt32(index);
+            }
+            else if (Strings.Count > byte.MaxValue)
+            {
+                bs.WriteUInt16((ushort)index);
+            }
+            else
+            {
+                bs.WriteByte((byte)index);
+            }
         }
 
         public void SerializeToTextXML(string outputPath)
         {
             using (XmlWriter writer = XmlWriter.Create(outputPath, new XmlWriterSettings() { Indent = true }))
             {
-                WriteNode(writer, RootNode);
+                WriteXmlNode(writer, RootNode);
             }
         }
 
-        private void WriteNode(XmlWriter writer, CXMLNode node)
+        private void WriteXmlNode(XmlWriter writer, CXMLNode node)
         {
             writer.WriteStartElement(node.Name);
 
             if (node.Attributes is not null)
             {
                 foreach (var attr in node.Attributes)
-                    writer.WriteAttributeString(attr.Key, attr.Value);
+                    writer.WriteAttributeString(attr.Name, attr.Value);
             }
 
             if (node.ChildNodes != null)
             {
                 foreach (var cnode in node.ChildNodes)
-                    WriteNode(writer, cnode);
+                    WriteXmlNode(writer, cnode);
             }
 
             writer.WriteEndElement();
             writer.Flush();
         }
 
-        public CXMLNode ReadNode(BinaryStream bs)
+        public CXMLNode ReadBinaryXmlNode(BinaryStream bs)
         {
             var node = new CXMLNode();
 
-            byte nodeFlags = bs.Read1Byte();
+            node.Flags = (CXmlNodeFlags)bs.Read1Byte();
             node.Name = ReadStringIndex(bs);
 
-            if ((nodeFlags & 0x02) != 0) // attributes
+            if (node.Flags.HasFlag(CXmlNodeFlags.HasAttributes)) // attributes
             {
                 byte attrCount = bs.Read1Byte();
                 node.Attributes = new List<CXMLAttribute>(attrCount);
 
                 for (var i = 0; i < attrCount; i++)
                 {
-                    var attr = new CXMLAttribute();
-                    attr.Key = ReadStringIndex(bs);
-                    attr.Value = ReadStringIndex(bs);
-                    node.Attributes.Add(attr);
+                    string key = ReadStringIndex(bs);
+                    string value = ReadStringIndex(bs);
+                    node.Attributes.Add(new CXMLAttribute(key, value));
                 }
             }
 
-            if ((nodeFlags & 0x04) != 0) // attributes
+            if (node.Flags.HasFlag(CXmlNodeFlags.HasChildNodes))
             {
                 short nodeCount = bs.ReadInt16();
                 node.ChildNodes = new List<CXMLNode>(nodeCount);
                 for (var i = 0; i < nodeCount; i++)
                 {
-                    var childNode = ReadNode(bs);
+                    var childNode = ReadBinaryXmlNode(bs);
                     node.ChildNodes.Add(childNode);
                 }
             }
@@ -124,18 +261,28 @@ namespace BXML
                 return Strings[idx];
             }
         }
+
+        private void AddString(string str)
+        {
+            if (!Strings.Contains(str))
+                Strings.Add(str);
+        }
     }
 
     public class CXMLNode
     {
-        public List<CXMLNode> ChildNodes { get; set; }
-        public List<CXMLAttribute> Attributes { get; set; }
+        public CXmlNodeFlags Flags { get; set; }
+        public List<CXMLNode> ChildNodes { get; set; } = new();
+        public List<CXMLAttribute> Attributes { get; set; } = new();
         public string Name { get; set; }
     }
 
-    public class CXMLAttribute
+    public record CXMLAttribute(string Name, string Value);
+
+    public enum CXmlNodeFlags : byte
     {
-        public string Key { get; set; }
-        public string Value { get; set; }
+        IsNode = 1 << 0,
+        HasAttributes = 1 << 1,
+        HasChildNodes = 1 << 2,
     }
 }
--- BXML/Program.cs
+++ BXML/Program.cs
@@ -1,16 +1,78 @@
-﻿namespace BXML
+﻿using System.Xml;
+
+namespace BXML
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Forza BXML <-> XML by Nenkai");
+            Console.WriteLine("- https://github.com/Nenkai");
+            Console.WriteLine("- https://twitter.com/Nenkaai");
+            Console.WriteLine("-----------------------------");
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: <bxml or xml files>");
+                Console.WriteLine("NOTE: Files will be automatically converted to binary or plain text");
+            }
+
             foreach (var arg in args)
             {
-                if (File.Exists(arg))
+                if (!File.Exists(arg))
                 {
-                    var bxml = BXML.ReadFile(arg);
-                    Console.WriteLine($"Processing '{arg}'");
-                    bxml.SerializeToTextXML(arg);
+                    Console.WriteLine($"Skipping '{arg}', file does not exist");
+                    continue;
+                }
+
+                using var fs = new FileStream(arg, FileMode.Open);
+                using var bs = new BinaryReader(fs);
+
+                if (fs.Length < 4)
+                {
+                    Console.WriteLine($"Skipping '{arg}', file too small");
+                    continue;
+                }
+
+                if (bs.ReadUInt32() == BXML.MAGIC)
+                {
+                    try
+                    {
+                        fs.Position = 0;
+                        var bxml = new BXML();
+                        bxml.LoadFromStream(fs);
+
+                        string output = arg + ".xml";
+                        bxml.SerializeToTextXML(arg + ".xml");
+
+                        Console.WriteLine($"Converted BXML '{arg}' -> XML");
+                    } 
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Failed to convert '{arg}' to XML - {e.Message}");
+                    }
+                }
+                else if (arg.EndsWith(".xml"))
+                {
+                    fs.Position = 0;
+
+                    try
+                    {
+                        XmlDocument document = new XmlDocument();
+                        document.Load(fs);
+
+                        var bxml = new BXML();
+                        bxml.CreateFromXmlNode(document);
+
+                        using var output = new FileStream(Path.ChangeExtension(arg, ".bxml"), FileMode.Create);
+                        bxml.Serialize(output);
+
+                        Console.WriteLine($"Converted XML '{arg}' -> BXML");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Failed to convert '{arg}' to BXML - {e.Message}");
+                    }
                 }
             }
         }
